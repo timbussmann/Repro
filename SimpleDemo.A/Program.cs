3279e19c1fc97d2874eb8e8ea5fe25f98470bbed
Tim Bussmann <tim.bussmann@particular.net>
2017-07-21 18:57:31 +0200

    simplify repro further

--- SimpleDemo.A/Program.cs
+++ SimpleDemo.A/Program.cs
@@ -1,55 +1,32 @@
 ﻿using System;
-using System.Configuration;
-using System.Diagnostics;
-using System.Globalization;
-using System.IO;
-using System.Runtime.Serialization.Json;
-using System.Threading;
 using System.Threading.Tasks;
 using NServiceBus;
-using NServiceBus.Logging;
 using SimpleDemo.Messages;
 
 namespace SimpleDemo.A
 {
     class Program
     {
-        // When started within VisualStudi, the application crashes immediately on Endpoint.Start
+        // When started within VisualStudio, the application crashes immediately on Endpoint.Start
         // When started via dotnet run, the application crashes when sending a message
         static async Task Main(string[] args)
         {
             // Setting the culture to en-US resolves the issue
-//            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
-//            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            // Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+            // Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
 
-            // This is the code in NServiceBus.Core triggering the exception. 
-            // Calling this code here makes the exception to go away
-            // but if FirstChanceExceptions are enabled, I notice FileLoadException when trying to load System.Private.CoreLib
-//            var serializer = new DataContractJsonSerializer(typeof(string));
-//            var memoryStream = new MemoryStream();
-//            serializer.WriteObject(memoryStream, "test");
+            // uncommenting this code resolves the issue as well:
+            // var serializer = new DataContractJsonSerializer(typeof(string));
+            // var memoryStream = new MemoryStream();
+            // serializer.WriteObject(memoryStream, "test");
 
             var endpointConfiguration = new EndpointConfiguration("SimpleDemo.A");
             var transport = endpointConfiguration.UseTransport<LearningTransport>();
-            transport.Routing().RouteToEndpoint(typeof(DemoCommand), "SimpleDemo.B");
 
             var endpoint = await Endpoint.Start(endpointConfiguration);
 
-            Console.WriteLine("Press [Esc] to exit, press any other key to send a message");
-            while (true)
-            {
-                switch (Console.ReadKey().Key)
-                {
-                    case ConsoleKey.Escape:
-                        return;
-                    default:
-                        var sendOptions = new SendOptions();
-                        sendOptions.RouteToThisEndpoint();
-                        await endpoint.Send(new DemoCommand(), sendOptions);
-                        Console.WriteLine("message sent");
-                        break;
-                }
-            }
+            await endpoint.SendLocal(new DemoCommand());
+            Console.ReadKey();
         }
     }
 }
